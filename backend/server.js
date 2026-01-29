c1b302a6b628e752bcf24687720a561af8a94eac
Ashad Ali <ashadaliii101@gmail.com>
2026-01-29 14:25:52 +0500

    triggering new build

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
