4bcc2fd86d5ed35219dc85fd3d26e2b7427de6e1
Ashad Ali <ashadaliii101@gmail.com>
2026-02-02 15:55:16 +0500

    Added DemoUser

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,9 +4,24 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
+
+    // --- Demo Access Bypass  ---
+    if (username === "admin" && password === "admin123") {
+        const token = jwt.sign(
+            { id: 'demo_admin_007' }, // Fake ID for demo
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' }
+        );
+        return res.json({ 
+            token, 
+            username: "admin", 
+            isDemo: true // Optional: frontend ko batane ke liye
+        });
+    }
+    // ------------------------------------------
+
     try {
         const user = await User.findOne({ username });
         if (!user) return res.status(400).json({ msg: "User not Found" });
@@ -16,7 +31,9 @@ router.post('/login', async (req, res) => {
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
         res.json({ token, username: user.username });
-    } catch (err) { res.status(500).send("Server Error"); }
+    } catch (err) { 
+        res.status(500).send("Server Error"); 
+    }
 });
 
 module.exports = router;
